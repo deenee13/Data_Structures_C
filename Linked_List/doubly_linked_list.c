1faeee14b509f08db483868bb5987ff47c3953c8
Deepen <deepen@deepens-air.lan>
2020-08-04 15:38:59 -0700

    Done with the Linked list

---
+++ Linked_List/doubly_linked_list.c
@@ -0,0 +1,104 @@
+// Implementation of the Doubly Linked list
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+struct node{
+    int data;
+    struct node* prev;
+    struct node* next;
+};
+
+// Function Declaration
+struct node* insert_head(int data, struct node* head); // Insert node at head
+struct node* get_new_node(int x);   // Function to create a new node
+void print_forward(struct node *head);  // Print Function in forward directiom
+void print_reverse(struct node *head);  // Print function in reverse direction
+
+
+
+// Function to create new node
+struct node* get_new_node(int x)
+{
+    struct node* newnode = (struct node*) malloc(sizeof(struct node));
+    newnode->data = x;
+    newnode->next = NULL;
+    newnode->prev = NULL;
+    return(newnode);
+}
+
+// Function to insert new node at head
+struct node* insert_head(int x, struct node* head)
+{
+    struct node* newnode; 
+    newnode = get_new_node(x);
+    ////printf("In insert head function\n");
+
+    // This if statement is important over here as if the list is empty then we will 
+    // access the prev location first in this function which will give segmentation fault
+    if(head == NULL)
+    {
+        head = newnode;
+        return(head);
+    }
+
+    /* Due to this line above if statement is important 
+     as this is not there in sinle linked list above if statement
+     in sinlge linkes list can be eliminated */
+    head->prev = newnode;
+
+    newnode->next = head;
+    head = newnode;
+    ////printf("Out of Insert head function\n");
+    return(head);
+}
+
+// Function to print linked list in forward direction 
+void print_forward(struct node* head)
+{
+
+  printf("Forward List is: ");
+  while (head != NULL)
+  {
+    printf("%d ", head->data);
+    head = head->next;
+  }
+  printf("\n");
+} 
+
+// Function to print linked list in reverse direction 
+void print_reverse(struct node* head)
+{
+    if(head == NULL)
+    {
+        return;
+    }
+    
+    while (head->next != NULL)
+    {
+        head = head->next;
+    }
+
+    printf("Reverse List is: ");
+    while (head != NULL)
+    {
+        printf("%d ", head->data);
+        head = head->prev;
+    }
+    printf("\n");
+}
+
+
+
+int main()
+{
+    struct node* head = NULL;   // Why is this statement so important
+    //printf("Addres of the *node head: %d\n",head);
+    head = insert_head(1,head); // Expected output: 1
+    head = insert_head(2,head); // Expected output: 2 1
+    head = insert_head(3,head); // Expected output: 3 2 1
+    print_forward(head);        // Expected output: 3 2 1
+    print_reverse(head);        // Expected output: 1 2 3
+
+}
--- Linked_List/insert_node_nth_position.c
+++ Linked_List/insert_node_nth_position.c
@@ -1,4 +1,4 @@
-// Inserting a node at Nth Position 
+// Inserting a node at Nth Position in linked list, deleting Nth node and also reversing the linked list 
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -10,11 +10,14 @@ struct node{
 };
 
 // Function Declaration 
-struct node* insert( int data, int position, struct node *head);
-void print(struct node *head);
+struct node* insert( int data, int position, struct node *head);   // Function to insert a node at nth location
+struct node* delete(int position, struct node *head);              // Function to delete a node at nth location  
+struct node* reverse(struct node *head);                           // Function to reverse a linked list 
+struct node* reverse_recursion(struct node *head);                           // Function to reverse a linked list using recursion
+void print(struct node *head); // Function to print the linked list 
 
 
-
+// Function to insert a node at nth location
 struct node* insert( int data, int position, struct node *head)
 {
     struct node* temp1 = (struct node*) malloc(sizeof(struct node));
@@ -46,6 +49,81 @@ struct node* insert( int data, int position, struct node *head)
 
 }
 
+// Function to delete a node at nth location  
+struct node* delete(int position, struct node *head)
+{
+    struct node* temp1 = head;
+    if(position == 1)
+    {
+        // Link has been established as head stores the address of the second node
+        head = temp1->next;
+        free(temp1);
+        return(head);
+    }
+
+    // this for loop will take you to n-1 location to get the link proper
+    // remember it is staritng fromn 0 that is why it is going till n-2 and not n-1 
+    // don't get confuse because of that 
+    for( int i=0; i<(position - 2); i++)
+    {
+        temp1 = temp1->next;
+    }
+
+    struct node* temp2 = temp1->next; // To go ot the nth node
+    temp1->next = temp2->next; // (n + 1)th node
+
+    // To free the space
+    free(temp2);
+    return(head);
+}
+
+// Function to reverse a linked list
+struct node* reverse(struct node *head)
+{
+    struct node* current;
+    struct node* prev;  // To store the value of the previous node to reverse the list 
+    struct node* next;  // to store the value of the next node to prevent loss of the address
+
+    current = head;
+    prev = NULL;
+
+    while(current != NULL)
+    {   
+        // First we will store the next field of current node as the address of the next node
+        next = current->next;
+
+        // Then we will update the current node's next field with the previous field which we have set at null over here
+        current->next = prev;
+
+        // Then we will update the prev and the current struct node to move forward and repeat the process
+        // by doing below statement we are basically changing the address of the particular node to point to specific location
+        prev = current;
+        current = next;
+    }
+
+    // To adjust the head of the linked list most important step 
+    head = prev;
+    return(head);
+}
+
+
+// Function to reverse a linked list using recursion 
+struct node* reverse_recursion(struct node *head)
+{
+    struct node* temp;
+    if(head->next == NULL)
+    {
+        temp = head;
+        return(temp);
+    }
+    reverse_recursion(head->next);
+    struct node* q = head->next;
+    q->next = head;
+    head->next = NULL;
+    return(temp);
+}
+
+// Function to print the linked list 
 void print(struct node*head)
 {
     struct node *temp = head;
@@ -63,10 +141,27 @@ void print(struct node*head)
 // You could also take values from the user
 int main()
 {
+    int position;
+
     struct node *head = NULL;    // empty list
     head = insert(2,1,head);    // list: 2
     head = insert(3,2,head);    // list: 2,3
     head = insert(4,1, head);    // list: 4,2,3
     head = insert(5,2,head);    // list: 4,5,2,3
+    printf("Complete linked list before deletion\n");
     print(head);
+
+    // Reversing a linked list 
+    printf(" Linked list after reversing it\n");
+    head = reverse(head);
+    print(head);
+
+    printf("Value of position to be deleted: ");
+    scanf("%d",&position);
+
+    head = delete(position,head);
+    printf("Complete linked list after deletion\n");
+    print(head);
+
+
 }
